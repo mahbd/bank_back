e1cb9f6cedfcf131315b478b3d60e71c49490551
Mahmudul Alam <mahmudula2000@gmail.com>
2022-05-24 11:13:49 +0600

    Primarily completed Transaction admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Transaction
+from constants import *
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'type', 'amount', 'status', 'created', 'action_button')
+    readonly_fields = ('created',)
+    list_per_page = 20
+    search_fields = ('id', 'user__username', 'amount', 'type', 'method')
+    list_filter = ('type', 'status', 'created')
+    autocomplete_fields = ('user', 'receiver')
+
+    def action_button(self, obj):
+        if obj.status == TRANSACTION_STATUS_PENDING:
+            return format_html(
+                '<a class="button" href="/admin/main/transaction/{}/approve/">Approve</a>'
+                '<a style="background-color: red; color: white; padding: 4px" href="/admin/main/transaction/{}/deny/">Deny</a>',
+                obj.id, obj.id)
+        return format_html(
+            '<a class="button" href="{}">{}</a>',
+            '/admin/main/transaction/{}/'.format(obj.id),
+            'View'
+        )
+
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
