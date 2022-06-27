5ecca22dd4437932df4bf82a8c9cef2ec14cb3bc
Mahmudul Alam <mahmudula2000@gmail.com>
2022-06-27 08:48:57 +0600

    Improved tests.py using request client instead of Client

--- api/tests.py
+++ api/tests.py
@@ -1,98 +1,126 @@
+from random import choices, randint
+from string import ascii_letters, digits
+
 from django.contrib.auth import get_user_model
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
+from rest_framework.test import RequestsClient
 
 from main.models import ExternalBank
 
-c = Client()
+c = RequestsClient()
 User = get_user_model()
+
+S_URL = 'http://testserver'
+
+
+def create_external_bank(user: User, name: str = None, acc_num: str = None, info: str = None) -> ExternalBank:
+    if not name:
+        name = ''.join(choices(ascii_letters, k=randint(5, 10)))
+    if not acc_num:
+        acc_num = ''.join(choices(digits, k=randint(5, 10)))
+    if not info:
+        info = ''.join(choices(ascii_letters, k=randint(50, 100)))
+    ex_bank = ExternalBank()
+    ex_bank.user = user
+    ex_bank.name = name
+    ex_bank.account_number = acc_num
+    ex_bank.information = info
+    ex_bank.save()
+    return ex_bank
+
+
+def create_user(username: str = None, password: str = None, **kwargs) -> tuple[User, str]:
+    if not username:
+        username = ''.join(choices(ascii_letters, k=randint(5, 10)))
+    if not password:
+        password = ''.join(choices(ascii_letters, k=randint(5, 10)))
+    user = User.objects.create_user(username=username, password=password, **kwargs)
+    response = c.post(S_URL + reverse('token_obtain_pair'), {'username': username, 'password': password})
+    token = response.json()['access']
+    return user, token
+
+
+def c_header(token: str = None, is_json: bool = True) -> dict:
+    """
+    Creates a header for a request
+    """
+    headers = {}
+    if token:
+        headers['Authorization'] = f'Bearer {token}'
+    if is_json:
+        headers['Content-Type'] = 'application/json'
+    return headers
 
 
 class ExternalBankTest(TestCase):
     def setUp(self) -> None:
-        self.api_url = reverse('external_bank-list')
+        self.api_url = S_URL + reverse('external_bank-list')
         # create a user
-        self.user = User.objects.create_user(username='test', password='test')
-        self.user2 = User.objects.create_user(username='test2', password='test2')
-        self.admin = User.objects.create_superuser(username='admin', password='admin')
-        self.token = c.post(reverse('token_obtain_pair'), {'username': 'test', 'password': 'test'}).data['access']
-        self.admin_token = c.post(reverse('token_obtain_pair'), {'username': 'admin', 'password': 'admin'}).data[
-            'access']
-        self.headers = {'HTTP_AUTHORIZATION': f'Bearer {self.token}', 'Content-Type': 'application/json'}
-        self.admin_headers = {'HTTP_AUTHORIZATION': f'Bearer {self.admin_token}', 'HTTP_Content-Type': 'application/json'}
-        # create an external bank
-        self.external_bank = ExternalBank()
-        self.external_bank.user = self.user
-        self.external_bank.name = 'test'
-        self.external_bank.account_number = 'test'
-        self.external_bank.information = 'test'
-        self.external_bank.save()
-        self.external_bank2 = ExternalBank()
-        self.external_bank2.user = self.user2
-        self.external_bank2.name = 'test2'
-        self.external_bank2.account_number = 'test2'
-        self.external_bank2.information = 'test2'
-        self.external_bank2.save()
+        self.user, self.token = create_user()
+        self.user2, self.token2 = create_user("test2", "test2")
+        self.admin, self.admin_token = create_user("admin", "admin", is_superuser=True, is_staff=True)
+        self.external_bank = create_external_bank(self.user, name='test')
+        self.external_bank2 = create_external_bank(self.user2, name='test2')
 
     def test_get_external_bank(self):
         response = c.get(self.api_url)
         self.assertEqual(response.status_code, 401)
 
     def test_get_external_bank_with_token(self):
-        response = c.get(self.api_url, **self.headers)
+        response = c.get(self.api_url, headers=c_header(self.token))
         self.assertEqual(response.status_code, 200)
-        data = response.data['results']
+        data = response.json()['results']
         self.assertEqual(len(data), 1)
         self.assertEqual(data[0]['name'], 'test')
         self.assertEqual(data[0]['user'], self.user.id)
 
     def test_get_external_bank_with_admin_token(self):
-        response = c.get(self.api_url,
-                         **{'HTTP_AUTHORIZATION': f'Bearer {self.admin_token}', 'Content-Type': 'application/json'})
+        response = c.get(self.api_url, headers=c_header(self.admin_token))
         self.assertEqual(response.status_code, 200)
-        data = response.data['results']
+        data = response.json()['results']
         self.assertEqual(len(data), 2)
         self.assertEqual(data[0]['name'], 'test')
         self.assertEqual(data[1]['name'], 'test2')
 
     def test_create_external_bank(self):
         response = c.post(self.api_url, {'name': 'test2', 'account_number': 'test2', 'information': 'test2'},
-                          **{'Content-Type': 'application/json'})
+                          headers=c_header())
         self.assertEqual(response.status_code, 401)
 
     def test_create_external_bank_with_token(self):
-        response = c.post(self.api_url, {'name': 'test2', 'account_number': 'test2', 'information': 'test2'},
-                          **self.headers)
-        self.assertEqual(response.status_code, 201, response.data)
-        data = response.data
+        response = c.post(self.api_url, data={"name": "test2", 'account_number': 'te', 'information': 'test2'},
+                          headers=c_header(self.token))
+        self.assertEqual(response.status_code, 201, response.json())
+        data = response.json()
         self.assertEqual(data['name'], 'test2')
         self.assertEqual(data['user'], self.user.id)
 
     def test_update_external_bank(self):
         response = c.put(f'{self.api_url}{self.external_bank.id}/',
                          {'name': 'test2', 'account_number': 'test2', 'information': 'test2'},
-                         **{'Content-Type': 'application/json'})
+                         headers=c_header())
         self.assertEqual(response.status_code, 401)
 
     def test_update_external_bank_with_token(self):
         response = c.patch(f'{self.api_url}{self.external_bank.id}/',
                            {'name': 'test2', 'account_number': 'test2', 'information': 'test2'},
-                           **self.headers)
+                           headers=c_header(self.token))
         self.assertEqual(response.status_code, 200)
-        data = response.data
+        data = response.json()
         self.assertEqual(data['name'], 'test2')
         self.assertEqual(data['user'], self.user.id)
 
     def test_update_external_bank_of_another_user(self):
         response = c.patch(f'{self.api_url}{self.external_bank2.id}/',
                            {'name': 'test1', 'account_number': 'test2', 'information': 'test2'},
-                           **self.headers)
+                           headers=c_header(self.token))
         self.assertEqual(response.status_code, 404)
 
     def test_update_external_bank_of_another_user_with_admin_token(self):
         response = c.patch(f'{self.api_url}{self.external_bank.id}/', {'name': 'test2'},
-                           **{'HTTP_AUTHORIZATION': f'Bearer {self.admin_token}'})
+                           headers=c_header(self.admin_token))
         self.assertEqual(response.status_code, 200)
-        data = response.data
+        data = response.json()
         self.assertEqual(data['name'], 'test2')
         self.assertEqual(data['user'], self.user.id)
